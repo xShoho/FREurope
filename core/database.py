817eb63f881de2b129ae441f821a85c0c451b877
Kamil Starosta <ks@g3w.eu>
2025-06-14 11:25:12 +0200

    Added error handling to database logic

--- core/database.py
+++ core/database.py
@@ -1,7 +1,12 @@
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 
 from config.settings import MONGO_URI, DB_NAME, COLLECTION_NAME
 from models.schemas import Flight
+from core.logger import init_logger
+
+
+logger = init_logger("database")
 
 
 client = MongoClient(MONGO_URI)
@@ -9,16 +14,31 @@ db = client[DB_NAME]
 collection = db[COLLECTION_NAME]
 
 
+try:
+    client.admin.command("ping")
+except ConnectionFailure:
+    logger.error("Server not available")
+
+
 def insert_data(data: Flight):
-    collection.insert_one(data.model_dump())
+    try:
+        collection.insert_one(data.model_dump())
+    except:
+        logger.warning(f"Couldn't insert element: { data.model_dump() }")
     
 
 def update_data(data: Flight):
-    collection.update_one({ "flight_uuid": data.flight_uuid }, { "$set": data.model_dump(exclude= "flight_uuid") })
+    try:
+        collection.update_one({ "flight_uuid": data.flight_uuid }, { "$set": data.model_dump(exclude= "flight_uuid") })
+    except:
+        logger.warning(f"Couldn't update element of flight_uuid: { data.flight_uuid }")
     
 
 def get_data_uuid(flight_uuid: str):
-    flight = collection.find_one({ "flight_uuid": flight_uuid })
+    try:
+        flight = collection.find_one({ "flight_uuid": flight_uuid })
+    except:
+        logger.warning(f"Couldn't find element of flight_uuid: { flight_uuid }")
     
     if flight:
         return flight
